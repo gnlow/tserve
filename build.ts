0801598ef9fad81926d617c42ab3f494154ef33c
Gnlow <gnlow@protonmail.com>
2024-02-17 00:28:40 +0900

    i think it works

--- build.ts
+++ build.ts
@@ -2,8 +2,7 @@ import { walk } from "https://deno.land/std@0.215.0/fs/walk.ts"
 import { transpile } from "./src/transpile.ts"
 
 for await (const entry of walk(".", { exts: ["ts"] })) {
-    const src = await Deno.readTextFile(entry.path)
-    const result = await transpile(src)
+    const result = await transpile(entry.path)
 
     if (!result) throw new Error()
 
