57cc77e31f875e563c5bef4e06885fb214275bd1
Gnlow <gnlow@protonmail.com>
2024-02-17 01:12:33 +0900

    hmm

--- build.ts
+++ build.ts
@@ -1,7 +1,7 @@
 import { walk } from "https://deno.land/std@0.215.0/fs/walk.ts"
 import { transpile } from "./src/transpile.ts"
 
-for await (const entry of walk(".", { exts: ["ts"] })) {
+for await (const entry of walk(".", { exts: [".ts"] })) {
     const result = await transpile(entry.path)
 
     await Deno.writeTextFile(entry.path, result || "")
